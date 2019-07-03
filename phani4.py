b9a9b41cb07ae295fa1c97eb070cad0fc3f68527
phaniteja21 <44160056+phaniteja21@users.noreply.github.com>
2019-07-03 11:37:57 +0530

    Add files via upload

---
+++ phani4.py
@@ -0,0 +1,8 @@
+a=input()
+if(a>="a" and a<="z"):
+  k=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","O","p","q","r","s","t","u","v","w","x","y","z"]
+  if a in k:
+    print("Alphabet")
+  else:
+    print("No")
+
